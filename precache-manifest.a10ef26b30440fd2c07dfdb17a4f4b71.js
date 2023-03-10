46e21de2ad251c61ea2a4684a0c3021faaaaf69b
Aryan <aryan.joy@outlook.com>
2023-03-09 22:54:50 -0700

    Updates

---
+++ precache-manifest.a10ef26b30440fd2c07dfdb17a4f4b71.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "f44e3f4e97343fd108aa",
+    "url": "/portfolio/static/css/main.2f4085bd.chunk.css"
+  },
+  {
+    "revision": "f44e3f4e97343fd108aa",
+    "url": "/portfolio/static/js/main.f44e3f4e.chunk.js"
+  },
+  {
+    "revision": "fdeb34be957d0625724f",
+    "url": "/portfolio/static/js/1.fdeb34be.chunk.js"
+  },
+  {
+    "revision": "5b0b79d8bebd06f81d03",
+    "url": "/portfolio/static/css/2.7d8acaaa.chunk.css"
+  },
+  {
+    "revision": "5b0b79d8bebd06f81d03",
+    "url": "/portfolio/static/js/2.5b0b79d8.chunk.js"
+  },
+  {
+    "revision": "430be7dc8ecccc7f7a5d",
+    "url": "/portfolio/static/js/runtime~main.430be7dc.js"
+  },
+  {
+    "revision": "3ba4464747cde1c115faf70967c6746e",
+    "url": "/portfolio/static/media/github.3ba44647.svg"
+  },
+  {
+    "revision": "f0da071279faf9befee64418706be16e",
+    "url": "/portfolio/static/media/linkedin.f0da0712.svg"
+  },
+  {
+    "revision": "fb9880a00de7afe895d7cd0d6914b3a0",
+    "url": "/portfolio/static/media/email.fb9880a0.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "6cd9f6ca9735ba7dd6fa45bb6a3c5b6f",
+    "url": "/portfolio/static/media/headerRect.6cd9f6ca.svg"
+  },
+  {
+    "revision": "50b98f02b80efc946d371df2c00d9984",
+    "url": "/portfolio/static/media/footerRect.50b98f02.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "080ad0d3ff795b17efec71c4dd20de8a",
+    "url": "/portfolio/static/media/twitter.080ad0d3.svg"
+  },
+  {
+    "revision": "731eff2abde5802c252eb4401e7b0414",
+    "url": "/portfolio/static/media/logo.731eff2a.svg"
+  },
+  {
+    "revision": "85af7c9f64302544485bfdb57b3bfbdb",
+    "url": "/portfolio/static/media/discord.85af7c9f.svg"
+  },
+  {
+    "revision": "e8257f2f5e202f185a1e7c78d8728dc2",
+    "url": "/portfolio/static/media/header-img.e8257f2f.svg"
+  },
+  {
+    "revision": "9ad810ca3d7d525c3dfd2cb75c3d97cc",
+    "url": "/portfolio/index.html"
+  }
+];
